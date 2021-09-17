0f0a29cc2c1e3470220cfa33c3920f262d46ab6d
Sugarplayer <90880846+Sugarplayer@users.noreply.github.com>
2021-09-17 10:10:57 +0800

    3字棋游戏主文件

---
+++ test.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include"game.h"
+//   | | 
+//  -----
+//   | | 
+//  -----
+//   | | 
+//
+int main()
+{
+    int a = 0;
+    menu();
+    do {
+        scanf("%d", &a);
+        switch (a)
+        {
+        case 1:
+            game();
+            break;
+        case 2:
+            break;
+        default:
+            printf("请重新选择\n");
+        }
+
+    } while (a != 2);
+
+    return 0;
+}
