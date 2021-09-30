cd2383e423766b37cfcef67615f0a95c88b447ba
Sugarplayer <90880846+Sugarplayer@users.noreply.github.com>
2021-09-30 10:35:52 +0800

    Create search.c

---
+++ search.c
@@ -0,0 +1,20 @@
+#include<studi.h>
+int count_bit_one(int a)
+{
+  int count = 1
+  {
+    if(a%2 ==1)
+    {
+      count++;
+    }
+    n = n/2;
+  }
+}
+int main()
+{
+  int a = 0;
+  scanf("%d",&a);
+  int count = count_bit_one(a);
+  printf("count=%d\n",count);
+  return 0;
+}
