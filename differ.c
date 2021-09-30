5188c69070a6698e993a58ca011777a7e5a2b095
Sugarplayer <90880846+Sugarplayer@users.noreply.github.com>
2021-09-30 11:21:05 +0800

    两个数的二进制表达式，有多少个不同位

---
+++ differ.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int get——diff(int m,int n)
+{
+int tmp = m^n;
+int count = 0;
+while(tmp)
+{
+tmp = tmp&(tmp-1);
+count++;
+}
+return count;
+}
+
+int main()
+{
+  int m = 0;
+  int n = 0;
+  scanf("%d%d",&m,&n);
+  int count = get——diff(m,n);
+  printf("%d",count);
+  
+  return 0;
+}
