2dd59aaa939f7964aa06caf3ccbdd736c8995277
Sugarplayer <90880846+Sugarplayer@users.noreply.github.com>
2021-09-30 10:59:55 +0800

    Update search.c

--- search.c
+++ search.c
@@ -19,3 +19,40 @@ int main()
   printf("count=%d\n",count);
   return 0;
 }
+//除了上述方法，可以把数字按位与1相与，将二进制序列所有位置都移动至最低位置
+
+/*
+int count_bit_one(int n)
+{
+  int count = 1
+  int i =0;
+  for(i = 0;i<32;i++)
+    {
+      if(((n>>i)&1)==1)
+    {
+      count++;
+    }
+    }
+
+    return count;
+}
+
+
+*/
+//除了上述方法，可以把数字n与数字n-1相与
+
+/*
+int count_bit_one(int n)
+{
+  int count = 1
+  while(n)
+    {
+    n = n &(n-1);
+    count++;
+    }
+
+    return count;
+}
+
+
+*/
