823827bf234d6e1f09019ef278856e1c1128713f
Sugarplayer <90880846+Sugarplayer@users.noreply.github.com>
2021-10-14 11:26:50 +0800

    计算一个数的次方

---
+++ Pow.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+double Pow(int n, int k)
+{
+    if (k < 0)
+        return 1.0 / (Pow(n, -k));
+    else if (k == 0)
+        return 1;
+    else
+        return n*Pow(n, k - 1);
+}
+int main()
+{
+    int n = 0;
+    int k = 0;
+    scanf("%d %d", &n, &k);
+    double c = 0;
+    c = Pow(n, k);
+    printf("%d的%d次方为：%f", n, k, c);
+    return 0;
+}
